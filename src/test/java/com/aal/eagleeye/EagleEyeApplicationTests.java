4e46696e9d04c968e4c3c943241eb29200095274
sharmila <sharmilajoshi19@gmail.com>
2022-01-10 16:24:18 +0530

    modified testclass

--- src/test/java/com/aal/eagleeye/EagleEyeApplicationTests.java
+++ src/test/java/com/aal/eagleeye/EagleEyeApplicationTests.java
@@ -1,9 +1,12 @@
 package com.aal.eagleeye;
 
 import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.aa.eagleeye.EagleEyeApplication;
+
+@SpringBootTest(classes = EagleEyeApplication.class)
 class EagleEyeApplicationTests {
 
 	@Test
